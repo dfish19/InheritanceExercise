e873dc5b86a060046ec2489f946bc3f6906304b3
dfish19 <dfish19@yahoo.com>
2022-08-15 17:27:02 -0500

    initial

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -29,11 +29,40 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
+            var b = new Bird();
+            b.Beak = 1;
+            b.Sleep = true;
+            b.Fly = true;
+            b.TwoLegs = true;
+
+
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+
+            var r = new Reptile()
+            {
+                HasTongue = true,
+                HasTail = true,
+                IsDangerous = true,
+                AreMammals = true,
+        };
+            var myAnimals = new Animal[] {b, r};
+
+            foreach (var item in myAnimals)
+            {
+                Console.WriteLine($"It is {item.Age} years old.");
+                Console.WriteLine($"The color of this animal is {item.Color}.");
+                Console.WriteLine($"The name of the animal is {item.Name}.");
+                Console.WriteLine($"The animal prefers to eat {item.Eat}.");
+                Console.WriteLine();
+            }
         }
+           
+	}
+       
     }
-}
+
+	
